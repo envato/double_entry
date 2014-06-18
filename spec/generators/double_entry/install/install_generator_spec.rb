6308de391a7484ca13e430a343c623c05a2bf21a
Stephanie Staub <steph.nacios@gmail.com>
2014-06-18 13:38:23 +1000

    Add install generator spec

---
+++ spec/generators/double_entry/install/install_generator_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+require 'action_controller'
+require 'generator_spec/test_case'
+require 'generators/double_entry/install/install_generator'
+
+
+describe DoubleEntry::Generators::InstallGenerator do
+  include GeneratorSpec::TestCase
+
+  destination File.expand_path("/tmp", __FILE__)
+
+  before do
+    prepare_destination
+    run_generator
+  end
+
+  specify do
+    expect(destination_root).to have_structure {
+      directory "db" do
+        directory "migrate" do
+          migration "create_double_entry_tables" do
+            contains 'class CreateDoubleEntryTable'
+            contains 'create_table "account_balances"'
+            contains 'create_table "lines"'
+            contains 'create_table "line_aggregates"'
+            contains 'create_table "line_checks"'
+          end
+        end
+      end
+    }
+  end
+
+end
