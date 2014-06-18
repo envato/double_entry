2de7f8a77bade347105f89f91823d084b80aa739
Stephanie Staub <steph.nacios@gmail.com>
2014-06-18 13:40:21 +1000

    add next_migration_number instead of extending ActiveRecord::Generators::Base, but defer to ActiveRecord

--- lib/generators/double_entry/install/install_generator.rb
+++ lib/generators/double_entry/install/install_generator.rb
@@ -6,9 +6,12 @@ module DoubleEntry
   module Generators
     class InstallGenerator < Rails::Generators::Base
       include Rails::Generators::Migration
-      extend ActiveRecord::Generators::Migration
 
       source_root File.expand_path('../templates', __FILE__)
+
+      def self.next_migration_number(path)
+        ActiveRecord::Generators::Base.next_migration_number(path)
+      end
 
       def copy_migrations
         migration_template "migration.rb", "db/migrate/create_double_entry_tables.rb"
