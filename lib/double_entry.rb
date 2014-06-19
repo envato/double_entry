593439a05dbdc8a3c1a37635c2fa84e2e4a34a0d
Anthony Sellitti <anthony.sellitti@envato.com>
2014-06-19 11:11:39 +1000

    slice responsibility off of interface and delegate to instance method

--- lib/double_entry.rb
+++ lib/double_entry.rb
@@ -13,6 +13,7 @@ require 'double_entry/configurable'
 
 require 'double_entry/account'
 require 'double_entry/account_balance'
+require 'double_entry/balance_calculator'
 
 require 'double_entry/aggregate'
 require 'double_entry/aggregate_array'
@@ -113,56 +114,7 @@ module DoubleEntry
 
     # Get the current balance of an account, as a Money object.
     def balance(account, args = {})
-      scope_arg = args[:scope] ? args[:scope].id.to_s : nil
-      scope = (account.is_a?(Symbol) ? scope_arg : account.scope_identity)
-      account = (account.is_a?(Symbol) ? account : account.identifier).to_s
-      from, to, at = args[:from], args[:to], args[:at]
-      code, codes = args[:code], args[:codes]
-
-      # time based scoping
-      conditions = if at
-        # lookup method could use running balance, with a order by limit one clause
-        # (unless it's a reporting call, i.e. account == symbol and not an instance)
-        ['account = ? and created_at <= ?', account, at] # index this??
-      elsif from and to
-        ['account = ? and created_at >= ? and created_at <= ?', account, from, to] # index this??
-      else
-        # lookup method could use running balance, with a order by limit one clause
-        # (unless it's a reporting call, i.e. account == symbol and not an instance)
-        ['account = ?', account]
-      end
-
-      # code based scoping
-      if code
-        conditions[0] << ' and code = ?' # index this??
-        conditions << code.to_s
-      elsif codes
-        conditions[0] << ' and code in (?)' # index this??
-        conditions << codes.collect { |c| c.to_s }
-      end
-
-      # account based scoping
-      if scope
-        conditions[0] << ' and scope = ?'
-        conditions << scope
-
-        # This is to work around a MySQL 5.1 query optimiser bug that causes the ORDER BY
-        # on the query to fail in some circumstances, resulting in an old balance being
-        # returned. This was biting us intermittently in spec runs.
-        # See http://bugs.mysql.com/bug.php?id=51431
-        if Line.connection.adapter_name.match /mysql/i
-          use_index = "USE INDEX (lines_scope_account_id_idx)"
-        end
-      end
-
-      if (from and to) or (code or codes)
-        # from and to or code lookups have to be done via sum
-        Money.new(Line.where(conditions).sum(:amount))
-      else
-        # all other lookups can be performed with running balances
-        line = Line.select("id, balance").from("#{Line.quoted_table_name} #{use_index}").where(conditions).order('id desc').first
-        line ? line.balance : Money.empty
-      end
+      DoubleEntry::BalanceCalculator.calculate(account, args)
     end
 
     # Identify the scopes with the given account identifier holding at least
---
+++ lib/double_entry/balance_calculator.rb
@@ -0,0 +1,78 @@
+# encoding: utf-8
+module DoubleEntry
+  class BalanceCalculator
+
+    def initialize(accout, scope, from, to, at, codes)
+      if account.is_a? Symbol
+        @account = account.to_s
+        @scope = args[:scope] ? args[:scope].id.to_s : nil
+      else
+        @account = account.identifier.to_s
+        @scope = account.scope_identity
+      end
+
+      @from = from
+      @to = to
+      @codes = codes
+    end
+
+
+    def self.calculate(account, args = {})
+      codes = [ args[:code] ] << args[:codes]
+      calculator = BalanceCalculator.new(account, args[:scope], args[:from], args[:to], args[:at], codes)
+      calculator.calculate
+    end
+
+    def calculate
+      # time based scoping
+      conditions = if at
+        # lookup method could use running balance, with a order by limit one clause
+        # (unless it's a reporting call, i.e. account == symbol and not an instance)
+        ['account = ? and created_at <= ?', account, at] # index this??
+      elsif from and to
+        ['account = ? and created_at >= ? and created_at <= ?', account, from, to] # index this??
+      else
+        # lookup method could use running balance, with a order by limit one clause
+        # (unless it's a reporting call, i.e. account == symbol and not an instance)
+        ['account = ?', account]
+      end
+
+      # code based scoping
+      if code
+        conditions[0] << ' and code = ?' # index this??
+        conditions << code.to_s
+      elsif codes
+        conditions[0] << ' and code in (?)' # index this??
+        conditions << codes.collect { |c| c.to_s }
+      end
+
+      # account based scoping
+      if scope
+        conditions[0] << ' and scope = ?'
+        conditions << scope
+
+        # This is to work around a MySQL 5.1 query optimiser bug that causes the ORDER BY
+        # on the query to fail in some circumstances, resulting in an old balance being
+        # returned. This was biting us intermittently in spec runs.
+        # See http://bugs.mysql.com/bug.php?id=51431
+        if Line.connection.adapter_name.match /mysql/i
+          use_index = "USE INDEX (lines_scope_account_id_idx)"
+        end
+      end
+
+      if (from and to) or (code or codes)
+        # from and to or code lookups have to be done via sum
+        Money.new(Line.where(conditions).sum(:amount))
+      else
+        # all other lookups can be performed with running balances
+        line = Line.select("id, balance").from("#{Line.quoted_table_name} #{use_index}").where(conditions).order('id desc').first
+        line ? line.balance : Money.empty
+      end
+    end
+
+  private
+
+    attr_reader :account, :scope, :from, :to, :at
+
+  end
+end
