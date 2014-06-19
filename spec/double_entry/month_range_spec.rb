3a02bd9132dd7a2695b0507a32a029d0de23a51f
Orien Madgwick <_@orien.io>
2014-06-19 21:24:29 +1000

    Remove spec from #reportable_months method
    I don't think we should have this method

--- spec/double_entry/month_range_spec.rb
+++ spec/double_entry/month_range_spec.rb
@@ -48,36 +48,4 @@ describe DoubleEntry::MonthRange do
       end
     end
   end
-
-  describe "#reportable_months" do
-    subject(:reportable_months) { DoubleEntry::MonthRange.current.reportable_months }
-
-    context "The date is 1st March 1970" do
-      before { Timecop.freeze(Time.new(1970, 3, 1)) }
-      it { should eq [
-        DoubleEntry::MonthRange.new(year: 1970, month: 1),
-        DoubleEntry::MonthRange.new(year: 1970, month: 2),
-        DoubleEntry::MonthRange.new(year: 1970, month: 3),
-      ] }
-    end
-
-    context "The date is 1st Jan 1970" do
-      before { Timecop.freeze(Time.new(1970, 1, 1)) }
-      it { should eq [ DoubleEntry::MonthRange.new(year: 1970, month: 1) ] }
-    end
-
-    context "Given a start time of 3rd Dec 1982" do
-      subject(:reportable_months) { DoubleEntry::MonthRange.current.reportable_months(from: Time.new(1982, 12, 3)) }
-
-      context "The date is 2nd Feb 1983" do
-        before { Timecop.freeze(Time.new(1983, 2, 2)) }
-        it { should eq [
-          DoubleEntry::MonthRange.new(year: 1982, month: 12),
-          DoubleEntry::MonthRange.new(year: 1983, month: 1),
-          DoubleEntry::MonthRange.new(year: 1983, month: 2),
-        ] }
-      end
-    end
-  end
-
 end
