a9fb47f1d1aed129c7f1f4dc24bbfb50a36cfd70
Anthony Sellitti <anthony.sellitti@envato.com>
2014-06-19 13:04:32 +1000

    ensure that it compacts code and codes properly

--- spec/double_entry/balance_calculator_spec.rb
+++ spec/double_entry/balance_calculator_spec.rb
@@ -12,9 +12,24 @@ describe DoubleEntry::BalanceCalculator do
       DoubleEntry::BalanceCalculator.calculate(anything, a_hash)
       expect(calculator).to have_received(:calculate)
     end
+
+    it 'compacts codes and code into a single array' do
+      DoubleEntry::BalanceCalculator.calculate(anything, {:code => 'code', :codes => ['code1', 'code2']})
+      expect(DoubleEntry::BalanceCalculator).to have_received(:new).with(anything, anything, anything, anything, anything, ['code1', 'code2', 'code'])
+    end
   end
 
   describe '#calculate' do
+    let(:scope) { nil }
+    let(:from) { nil }
+    let(:to) { nil }
+    let(:at) { nil }
+    let(:codes) { nil }
 
+    subject(:calculator) { DoubleEntry::BalanceCalculator.new(account, scope, from, to, at, codes) }
+
+    context 'with a scope specified' do
+
+    end
   end
 end
