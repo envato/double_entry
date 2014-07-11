6dac79813268ead27caf720f2db5d91c110dd9c2
Orien Madgwick <_@orien.io>
2014-07-12 01:46:57 +1000

    1 million dollars

--- lib/double_entry/reporting.rb
+++ lib/double_entry/reporting.rb
@@ -126,13 +126,14 @@ module DoubleEntry
     #
     # @example Find users with at least $1,000,000 in their savings accounts
     #   DoubleEntry::Reporting.scopes_with_minimum_balance_for_account(
-    #     Money.new(1_000_000_00),
-    #     :savings
-    #   ) # might return user ids: [ 1423, 12232, 34729 ]
+    #     1_000_000.dollars,
+    #     :savings,
+    #   ) # might return the user ids: [ 1423, 12232, 34729 ]
     # @param [Money] minimum_balance Minimum account balance a scope must have
     #   to be included in the result set.
     # @param [Symbol] account_identifier 
     # @return [Array<Fixnum>] Scopes
+    #
     def scopes_with_minimum_balance_for_account(minimum_balance, account_identifier)
       select_values(sanitize_sql_array([<<-SQL, account_identifier, minimum_balance.cents])).map {|scope| scope.to_i }
         SELECT scope
@@ -147,6 +148,7 @@ module DoubleEntry
     # @api private
     # @return [Boolean] true if all the amounts for an account add up to the final balance,
     #   which they always should.
+    #
     def reconciled?(account)
       scoped_lines = Line.where(:account => "#{account.identifier}", :scope => "#{account.scope}")
       sum_of_amounts = scoped_lines.sum(:amount)
