cb89aaf06678fa2f730c38363f5c35b0b6f2c64d
Orien Madgwick <_@orien.io>
2014-07-12 01:16:57 +1000

    reorder param type and name

--- lib/double_entry/reporting.rb
+++ lib/double_entry/reporting.rb
@@ -39,12 +39,11 @@ module DoubleEntry
     #     scope :ten_dollar_transfers, -> { where(:amount => 10_00) }
     #   end
     #   DoubleEntry::Reporting.aggregate(:sum, :checking, :save, :range => time_range, :filter => [:ten_dollar_transfers])
-    #
-    # @param function [Symbol] The function to perform on the set of transfers.
+    # @param [Symbol] function The function to perform on the set of transfers.
     #   Valid functions are :sum, :count, and :average
-    # @param account [Symbol] The symbol identifying the account to perform
+    # @param [Symbol] account The symbol identifying the account to perform
     #   the aggregate calculation on. As specified in the account configuration.
-    # @param code [Symbol] The application specific code for the type of
+    # @param [Symbol] code The application specific code for the type of
     #   transfer to perform an aggregate calculation on. As specified in the
     #   transfer configuration.
     # @option options :range [DoubleEntry::TimeRange] Only include transfers
