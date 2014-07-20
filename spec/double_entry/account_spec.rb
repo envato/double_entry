7c4ba9e20fc5b877ead019e6f5e5d319bc52413f
Orien Madgwick <_@orien.io>
2014-07-21 09:00:11 +1000

    namespace the spec

--- spec/double_entry/account_spec.rb
+++ spec/double_entry/account_spec.rb
@@ -1,35 +1,36 @@
 # encoding: utf-8
 require 'spec_helper'
-describe DoubleEntry::Account do
-  let(:identity_scope) { ->(value) { value } }
+module DoubleEntry
+  describe Account do
+    let(:identity_scope) { ->(value) { value } }
 
-  describe DoubleEntry::Account::Instance do
-    it "is sortable" do
-      account = DoubleEntry::Account.new(:identifier => "savings", :scope_identifier => identity_scope)
-      a = DoubleEntry::Account::Instance.new(:account => account, :scope => "123")
-      b = DoubleEntry::Account::Instance.new(:account => account, :scope => "456")
-      expect([b, a].sort).to eq [a, b]
+    describe Account::Instance do
+      it "is sortable" do
+        account = Account.new(:identifier => "savings", :scope_identifier => identity_scope)
+        a = Account::Instance.new(:account => account, :scope => "123")
+        b = Account::Instance.new(:account => account, :scope => "456")
+        expect([b, a].sort).to eq [a, b]
+      end
+
+      it "is hashable" do
+        account = Account.new(:identifier => "savings", :scope_identifier => identity_scope)
+        a1 = Account::Instance.new(:account => account, :scope => "123")
+        a2 = Account::Instance.new(:account => account, :scope => "123")
+        b  = Account::Instance.new(:account => account, :scope => "456")
+
+        expect(a1.hash).to eq a2.hash
+        expect(a1.hash).to_not eq b.hash
+      end
     end
 
-    it "is hashable" do
-      account = DoubleEntry::Account.new(:identifier => "savings", :scope_identifier => identity_scope)
-      a1 = DoubleEntry::Account::Instance.new(:account => account, :scope => "123")
-      a2 = DoubleEntry::Account::Instance.new(:account => account, :scope => "123")
-      b  = DoubleEntry::Account::Instance.new(:account => account, :scope => "456")
-
-      expect(a1.hash).to eq a2.hash
-      expect(a1.hash).to_not eq b.hash
-    end
-  end
-
-  describe DoubleEntry::Account::Set do
-    describe "#define" do
-      context "given a 'savings' account is defined" do
-        before { subject.define(:identifier => "savings") }
-        its(:first) { should be_a DoubleEntry::Account }
-        its("first.identifier") { should eq "savings" }
+    describe Account::Set do
+      describe "#define" do
+        context "given a 'savings' account is defined" do
+          before { subject.define(:identifier => "savings") }
+          its(:first) { should be_an Account }
+          its("first.identifier") { should eq "savings" }
+        end
       end
     end
   end
 end
-
