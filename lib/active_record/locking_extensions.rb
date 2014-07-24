25970f2318f9539392ccf37f9e531945fe266cc8
Eaden McKee <eaden@coinjar.com>
2014-07-24 13:50:33 +1000

    extract out methods from create_ignoring_duplicates

--- lib/active_record/locking_extensions.rb
+++ lib/active_record/locking_extensions.rb
@@ -20,7 +20,7 @@ module ActiveRecord
       begin
         yield
       rescue ActiveRecord::StatementInvalid => exception
-        if exception.message =~ /deadlock/i
+        if exception.message =~ /deadlock/i || exception.message =~ /database is locked/i
           raise ActiveRecord::RestartTransaction
         else
           raise
@@ -29,19 +29,41 @@ module ActiveRecord
     end
 
     # Create the record, but ignore the exception if there's a duplicate.
+    # if there is a deadlock, retry
     def create_ignoring_duplicates!(*args)
+      retry_deadlocks do
+        ignoring_duplicates do
+          create!(*args)
+        end
+      end
+    end
+
+    private
+
+    def ignoring_duplicates
       # Error examples:
-      #   PG::Error: ERROR:  deadlock detected
-      #   Mysql::Error: Deadlock found when trying to get lock
       #   PG::Error: ERROR:  duplicate key value violates unique constraint
       #   Mysql2::Error: Duplicate entry 'keith' for key 'index_users_on_username': INSERT INTO `users...
       #   ActiveRecord::RecordNotUnique  SQLite3::ConstraintException: column username is not unique: INSERT INTO "users"...
       begin
-        create!(*args)
+        yield
       rescue ActiveRecord::StatementInvalid, ActiveRecord::RecordNotUnique => exception
-        if  exception.message =~ /duplicate/i || exception.message =~ /is not unique/i
+        if  exception.message =~ /duplicate/i || exception.message =~ /(is|are) not unique/i
           # Just ignore it...someone else has already created the record.
-        elsif  exception.message =~ /deadlock/i || exception.message =~ /database is locked/i
+        else
+          raise
+        end
+      end
+    end
+
+    def retry_deadlocks
+      # Error examples:
+      #   PG::Error: ERROR:  deadlock detected
+      #   Mysql::Error: Deadlock found when trying to get lock
+      begin
+        yield
+      rescue ActiveRecord::StatementInvalid, ActiveRecord::RecordNotUnique => exception
+        if exception.message =~ /deadlock/i || exception.message =~ /database is locked/i
           # Somebody else is in the midst of creating the record. We'd better
           # retry, so we ensure they're done before we move on.
           retry
@@ -50,7 +72,6 @@ module ActiveRecord
         end
       end
     end
-
   end
 
   # Raise this inside a restartable_transaction to retry the transaction from the beginning.
