2433d0926d41164b1623c2ede2efd51ce7f7137d
Orien Madgwick <_@orien.io>
2015-01-11 10:51:23 +1100

    don't use should

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -55,8 +55,6 @@ RSpec.configure do |config|
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
-    mocks.syntax = [:expect, :should]
-
     # Prevents you from mocking or stubbing a method that does not exist on
     # a real object. This is generally recommended, and will default to
     # `true` in RSpec 4.
