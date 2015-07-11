43a686f94fda79cc32e30eda51683a4fa3e467c1
Orien Madgwick <_@orien.io>
2015-07-11 18:39:18 +1000

    Don't warn about potential false positives
    less warnings for now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -54,6 +54,7 @@ RSpec.configure do |config|
     # ...rather than:
     #   # => "be bigger than 2"
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    expectations.warn_about_potential_false_positives = false
   end
 
   # rspec-mocks config goes here. You can use an alternate test double
