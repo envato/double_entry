1ddd689f1d55ae38039f75badd5a33d282c00cf3
Anthony Sellitti <anthony.sellitti@envato.com>
2015-07-27 12:33:48 +1000

    lets place some coverage around Line Aggregates so that we can easier implement metadata filtering

--- spec/double_entry/reporting/line_aggregate_spec.rb
+++ spec/double_entry/reporting/line_aggregate_spec.rb
@@ -1,11 +1,83 @@
 # encoding: utf-8
-module DoubleEntry
-  module Reporting
-    RSpec.describe LineAggregate do
-      describe '.table_name' do
-        subject { LineAggregate.table_name }
-        it { should eq('double_entry_line_aggregates') }
+RSpec.describe DoubleEntry::Reporting::LineAggregate do
+  describe '.table_name' do
+    subject { DoubleEntry::Reporting::LineAggregate.table_name }
+    it { should eq('double_entry_line_aggregates') }
+  end
+
+  describe '#aggregate' do
+    let(:function) { :sum }
+    let(:account) { :account }
+    let(:code) { :transfer_code }
+    let(:named_scopes) do
+      [
+        :monkey_patched_method,
+        { :monkey_patched_method_with_param => :parameter }
+      ]
+    end
+    let(:start) { Time.parse('2014-07-27 10:55:44 +1000') }
+    let(:finish) { Time.parse('2015-07-27 10:55:44 +1000') }
+    let(:range) do
+      instance_double(DoubleEntry::Reporting::MonthRange,
+        :start => start,
+        :finish => finish,
+      )
+    end
+
+    let(:lines_scope) { spy(DoubleEntry::Line) }
+
+    subject(:aggregate) do
+      DoubleEntry::Reporting::LineAggregate.aggregate(
+        function, account, code, range, named_scopes
+      )
+    end
+
+    before do
+      stub_const('DoubleEntry::Line', lines_scope)
+
+      allow(lines_scope).to receive(:where).and_return(lines_scope)
+      allow(lines_scope).to receive(:monkey_patched_method).and_return(lines_scope)
+      allow(lines_scope).to receive(:monkey_patched_method_with_param).and_return(lines_scope)
+
+      aggregate
+    end
+
+    it 'performs the aggregation on the retrieved lines' do
+      expect(DoubleEntry::Line).to have_received(:sum).with(:amount)
+    end
+
+    context 'with named scopes specified' do
+      let(:named_scopes) do
+        [
+          :monkey_patched_method,
+          { :monkey_patched_method_with_param => :parameter }
+        ]
+      end
+
+      it 'filters by all the named scopes provided' do
+        expect(DoubleEntry::Line).to have_received(:monkey_patched_method)
+        expect(DoubleEntry::Line).to have_received(:monkey_patched_method_with_param).with(:parameter)
       end
     end
+
+    context 'with a code specified' do
+      let(:code) { :transfer_code }
+
+      it 'retrieves the appropriate lines for aggregation' do
+        expect(DoubleEntry::Line).to have_received(:where).with(:account => account)
+        expect(DoubleEntry::Line).to have_received(:where).with(:created_at => start..finish)
+        expect(DoubleEntry::Line).to have_received(:where).with(:code => code)
+      end
+    end
+
+    context 'with no code specified' do
+      let(:code) { nil }
+
+      it 'retrieves the appropriate lines for aggregation' do
+        expect(DoubleEntry::Line).to have_received(:where).with(:account => account)
+        expect(DoubleEntry::Line).to have_received(:where).with(:created_at => start..finish)
+      end
+    end
+
   end
 end
