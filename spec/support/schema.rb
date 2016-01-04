49bb8a6d3974aa95ce68e35659ffc8070368ac74
Stephanie Staub <steph.nacios@gmail.com>
2016-01-04 15:14:58 +1100

    Add partner_account column to double_entry_line_aggregates table

--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -31,19 +31,20 @@ ActiveRecord::Schema.define do
   add_index "double_entry_lines", ["scope", "account", "id"],         :name => "lines_scope_account_id_idx"
 
   create_table "double_entry_line_aggregates", :force => true do |t|
-    t.string     "function",   :limit => 15, :null => false
-    t.string     "account",    :limit => 31, :null => false
-    t.string     "code",       :limit => 47
-    t.string     "scope",      :limit => 23
+    t.string     "function",        :limit => 15, :null => false
+    t.string     "account",         :limit => 31, :null => false
+    t.string     "code",            :limit => 47
+    t.string     "partner_account", :limit => 31
+    t.string     "scope",           :limit => 23
     t.integer    "year"
     t.integer    "month"
     t.integer    "week"
     t.integer    "day"
     t.integer    "hour"
-    t.integer    "amount",                   :null => false
+    t.integer    "amount",                        :null => false
     t.string     "filter"
-    t.string     "range_type", :limit => 15, :null => false
-    t.timestamps                             :null => false
+    t.string     "range_type",      :limit => 15, :null => false
+    t.timestamps                                  :null => false
   end
 
   add_index "double_entry_line_aggregates", ["function", "account", "code", "year", "month", "week", "day"], :name => "line_aggregate_idx"
