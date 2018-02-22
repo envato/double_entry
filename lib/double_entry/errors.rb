8133a3ce1c91e94ef849ca0e015a9649e4e77b0b
kelseydh <kelseyh@gmail.com>
2018-02-22 08:38:42 +0000

    Introduce DoubleEntryError superclass

--- lib/double_entry/errors.rb
+++ lib/double_entry/errors.rb
@@ -1,15 +1,16 @@
 # encoding: utf-8
 module DoubleEntry
-  class UnknownAccount < RuntimeError; end
-  class AccountIdentifierTooLongError < RuntimeError; end
-  class ScopeIdentifierTooLongError < RuntimeError; end
-  class TransferNotAllowed < RuntimeError; end
-  class TransferIsNegative < RuntimeError; end
-  class TransferCodeTooLongError < RuntimeError; end
-  class DuplicateAccount < RuntimeError; end
-  class DuplicateTransfer < RuntimeError; end
-  class AccountWouldBeSentNegative < RuntimeError; end
-  class AccountWouldBeSentPositiveError < RuntimeError; end
-  class MismatchedCurrencies < RuntimeError; end
-  class MissingAccountError < RuntimeError; end
+  class DoubleEntryError < RuntimeError; end
+  class UnknownAccount < DoubleEntryError; end
+  class AccountIdentifierTooLongError < DoubleEntryError; end
+  class ScopeIdentifierTooLongError < DoubleEntryError; end
+  class TransferNotAllowed < DoubleEntryError; end
+  class TransferIsNegative < DoubleEntryError; end
+  class TransferCodeTooLongError < DoubleEntryError; end
+  class DuplicateAccount < DoubleEntryError; end
+  class DuplicateTransfer < DoubleEntryError; end
+  class AccountWouldBeSentNegative < DoubleEntryError; end
+  class AccountWouldBeSentPositiveError < DoubleEntryError; end
+  class MismatchedCurrencies < DoubleEntryError; end
+  class MissingAccountError < DoubleEntryError; end
 end
