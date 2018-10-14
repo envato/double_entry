28f917eabb0ebfe29cf7efd082720d1a4bbaa0db
Orien Madgwick <_@orien.io>
2018-10-14 14:34:18 +1100

    Use `references` to use Rails default id column type
    In Rails 5.1 the default type for row ids changed from integer to
    bigint.

--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -17,11 +17,10 @@ ActiveRecord::Schema.define do
     t.string     "code",            :null => false
     t.integer    "amount",          :null => false
     t.integer    "balance",         :null => false
-    t.integer    "partner_id"
+    t.references "partner",                         :index => false
     t.string     "partner_account", :null => false
     t.string     "partner_scope"
-    t.integer    "detail_id"
-    t.string     "detail_type"
+    t.references "detail",                          :index => false, :polymorphic => true
     t.timestamps                    :null => false
   end
 
@@ -50,7 +49,7 @@ ActiveRecord::Schema.define do
   add_index "double_entry_line_aggregates", ["function", "account", "code", "partner_account", "year", "month", "week", "day"], :name => "line_aggregate_idx"
 
   create_table "double_entry_line_checks", :force => true do |t|
-    t.integer    "last_line_id", :null => false
+    t.references "last_line",    :null => false, :index => false
     t.boolean    "errors_found", :null => false
     t.text       "log"
     t.timestamps                 :null => false
@@ -59,7 +58,7 @@ ActiveRecord::Schema.define do
   add_index "double_entry_line_checks", ["created_at", "last_line_id"], :name => "line_checks_created_at_last_line_id_idx"
 
   create_table "double_entry_line_metadata", :force => true do |t|
-    t.integer    "line_id", :null => false
+    t.references "line",    :null => false, :index => false
     t.string     "key",     :null => false
     t.string     "value",   :null => false
     t.timestamps            :null => false
