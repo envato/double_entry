61e4a9e8d2279503e4b326f4cb73c441f671af71
Orien Madgwick <_@orien.io>
2020-05-25 08:59:41 +1000

    Process database.yml configuration via ERB

--- spec/support/database.rb
+++ spec/support/database.rb
@@ -1,10 +1,12 @@
 require 'active_record'
 require 'database_cleaner'
+require 'erb'
+require 'yaml'
 
 FileUtils.mkdir_p 'tmp'
 
 db_engine = ENV['DB'] || 'mysql'
-database_config_file = File.expand_path('../database.yml', __FILE__)
+database_config_file = File.join(__dir__, 'database.yml')
 
 raise <<-MSG.strip_heredoc unless File.exist?(database_config_file)
   Please configure your spec/support/database.yml file.
@@ -12,7 +14,10 @@ raise <<-MSG.strip_heredoc unless File.exist?(database_config_file)
 MSG
 
 ActiveRecord::Base.belongs_to_required_by_default = true if ActiveRecord.version.version >= '5'
-ActiveRecord::Base.establish_connection(YAML.load_file(database_config_file)[db_engine])
+database_config_raw = File.read(database_config_file)
+database_config_yaml = ERB.new(database_config_raw).result
+database_config = YAML.load(database_config_yaml)
+ActiveRecord::Base.establish_connection(database_config[db_engine])
 
 RSpec.configure do |config|
   config.before(:suite) do
